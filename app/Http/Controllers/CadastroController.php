395468c52d8fa478a18c6f687d3c8199b740a1c1
Matheus Delmunde <matheusdelmunde@hotmail.com>
2020-10-30 22:22:35 -0300

    Api Finalizada

--- app/Http/Controllers/CadastroController.php
+++ app/Http/Controllers/CadastroController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Cadastro;
 
 class CadastroController extends Controller
 {
@@ -13,38 +14,29 @@ class CadastroController extends Controller
     }
 
     
-    public function create()
-    {
-    
-    }
+    public function create(){}
 
-    
     public function store(Request $request)
     {
-    
+        return Cadastro::create($request->all()); 
     }
 
-    
     public function show($id)
     {
-    
+        return Cadastro::findOrFail($id);
     }
 
-  
-    public function edit($id)
-    {
-
-    }
-
+    public function edit($id){}
     
     public function update(Request $request, $id)
     {
-    
+        $Cadastro = Cadastro::findOrFail($id);
+        return $Cadastro->update($request->all());
     }
 
-    
     public function destroy($id)
     {
-    
+        $Cadastro = Cadastro::findOrFail($id);
+        return $Cadastro->delete();
     }
 }
